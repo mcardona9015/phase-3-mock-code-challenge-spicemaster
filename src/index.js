5fd0401af5e3214ca2a13f9e3defcccd8a547723
mcardona9015 <73857382+mcardona9015@users.noreply.github.com>
2021-02-10 09:34:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // write your code here
+url = 'http://localhost:3000/spiceblends'
+
+fetchSpiceBlends()
+
+let spiceBlends
+function fetchSpiceBlends() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
